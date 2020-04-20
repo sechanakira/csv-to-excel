73685e0f78ac9424aa81a1c865d26444ae82b420
Shingirai Chanakira <sechanakira@yahoo.com>
2020-04-20 14:51:58 +0200

    Initial Commit

---
+++ src/main/java/com/github/sechanakira/csvtoexcel/CsvToExcelApplication.java
@@ -0,0 +1,13 @@
+package com.github.sechanakira.csvtoexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvToExcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sechanakira/csvtoexcel/CsvToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sechanakira.csvtoexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvToExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
